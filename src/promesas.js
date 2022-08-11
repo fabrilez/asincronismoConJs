cf76e2d8e81f96e0912cc3f9447e34f62af3c101
fabrilez <fabrilez2001@gmail.com>
2022-08-10 22:52:45 -0400

    trabajando con Promesas

---
+++ src/promesas.js
@@ -0,0 +1,21 @@
+const promesa = new Promise((resolve, reject) => {
+    resolve('Todo correcto')
+})
+
+const cows = 15;
+
+const countCows = new Promise((resolve, reject) => {
+    if (cows > 10) {
+        resolve(`We have ${cows} cows on the farms`)
+    }else {
+        reject("without cows enoght")
+    }
+});
+
+countCows.then((response) => {
+    console.log(response)
+})
+    .catch((error) => {
+        console.log(error)
+    })
+    .finally(() => console.log('finally'))
